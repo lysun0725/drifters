334eb46925c4151a416a5093f035200f7395ec19
mjh <Matthew.Harrison@noaa.gov>
2017-03-27 12:13:10 -0400

    updates to initialize MOM6 varibles

--- particle_driver.F90
+++ particle_driver.F90
@@ -2,11 +2,24 @@ PROGRAM particle_driver
 
   use MOM_file_parser, only : get_param, log_param, param_file_type
   use MOM_get_input, only : get_MOM_input, directories
-  use MOM_domains, only : MOM_domains_init
+  use MOM_domains, only : MOM_domains_init, MOM_infra_init, clone_MOM_domain, create_group_pass
   use MOM_grid, only : ocean_grid_type, MOM_grid_init
-  use MOM_io, only : slasher, MOM_io_init
+  use MOM_io, only : slasher, MOM_io_init, ASCII_FILE, READONLY_FILE
+  use MOM_io, only : check_nml_error, file_exists, open_file, close_file, io_infra_init
+  use MOM_error_handler, only : FATAL, WARNING, MOM_error
   use MOM_hor_index, only : hor_index_type, hor_index_init
-  use time_manager_mod, only: time_type
+  use MOM_grid, only : ocean_grid_type
+  use MOM_grid_initialize, only : set_grid_metrics
+  use MOM_fixed_initialization, only : MOM_initialize_fixed, MOM_initialize_topography
+  use MOM_coord_initialization, only : MOM_initialize_coord
+  use MOM_transcribe_grid,       only : copy_dyngrid_to_MOM_grid, copy_MOM_grid_to_dyngrid
+  use MOM_dyn_horgrid, only : dyn_horgrid_type, create_dyn_horgrid, destroy_dyn_horgrid
+  use MOM_verticalGrid, only : verticalGrid_type, verticalGridInit
+  use MOM, only : MOM_control_struct
+  use MOM_state_initialization, only : MOM_initialize_state
+  use time_manager_mod, only: time_type, set_time, set_date, JULIAN, NOLEAP, NO_CALENDAR, set_calendar_type
+  use ensemble_manager_mod, only : get_ensemble_size, ensemble_manager_init, ensemble_pelist_setup
+  use mpp_mod, only : set_current_pelist => mpp_set_current_pelist
 !  use particles_mod, only : particles_run, particles_save_restart
 !  use particles_io, only : read_restart_particles
 !  use particles_framework, only : particle, particles, particles_gridded
@@ -15,15 +28,15 @@ PROGRAM particle_driver
   implicit none
 
   type(ocean_grid_type), target :: Grid
-  type(ocean_vertical_grid_type), target :: GV
+  type(verticalGrid_type), target :: GV
   type(dyn_horgrid_type), pointer :: dG => NULL()
-  type(MOM_control_struct), target, dimension(:) :: CSp !<pointer of ensemble list of MOM control structures
-  type(MOM_control_struct), pointer :: CS=NULL() !<pointer to an element in CSp
+  type(MOM_control_struct), pointer, dimension(:) :: CSp !<pointer of ensemble list of MOM control structures
+  type(MOM_control_struct), pointer :: CS=> NULL() !<pointer to an element in CSp
   type(hor_index_type)   :: HI ! A hor_index_type for array extents
-  type(particles), target  :: drifters
-  type(particles), pointer :: node=>NULL() !<pointer to an element in drifters list
-  type(particles_gridded),pointer :: grd
-  type(time_type) :: time, start_time, end_time
+!  type(particles), target  :: drifters
+!  type(particles), pointer :: node=>NULL() !<pointer to an element in drifters list
+!  type(particles_gridded),pointer :: grd
+  type(time_type) :: time, time_start, time_start_segment, time_end, time_in
   real :: time_step
   character(len=128) :: history_file
   character(len=32)  :: topo_file
@@ -39,22 +52,27 @@ PROGRAM particle_driver
   integer :: calendar_type=-1
   character(len=16) :: month='jan',calendar='julian'
   logical :: concurrent = .false. !<use concurrent PE execution of model components
-  integer :: date_init(6), years=0, months=0, days=0, hours=0, minutes=0, seconds=0
+  integer :: date_init(6), date(6), years=0, months=0, days=0, hours=0, minutes=0, seconds=0
   integer :: yr, mon, day, hr, min, sec
+  integer :: is, ie, js, je, isd, ied, jsd, jed, IscB, IecB, JscB, JecB, IsdB, IedB, JsdB, JedB
+  integer :: ierr, io_status, n, nz, unit
 
   namelist /particle_driver_nml/ date_init, calendar, months, days, hours, minutes, seconds
 
   call MOM_infra_init(); call io_infra_init()
   call ensemble_manager_init(); ens_info = get_ensemble_size()
-  ens_size=ens_info(1); nPEs_ocn=ens_info(2); nPEs_atm=0; nPEs_lnd=0; nPEs_ice=0
+  ens_size=ens_info(1); nPEs_ocn=ens_info(2); nPEs_atm=nPEs_ocn; nPEs_lnd=nPEs_ocn; nPEs_ice=nPEs_ocn
+  allocate(ocn_pelist(nPEs_ocn))
+  allocate(atm_pelist(nPEs_ocn))
+  allocate(lnd_pelist(nPEs_ocn))
+  allocate(ice_pelist(nPEs_ocn))
 
   call ensemble_pelist_setup(concurrent,nPEs_atm,nPES_ocn,nPES_lnd,npes_ice,&
        atm_pelist,ocn_pelist,lnd_pelist,ice_pelist)
-  allocate(ocn_pelist(nPEs_ocn))
   call set_current_pelist(ocn_pelist)
-  allocate(CS(nPEs_ocn))
+  allocate(CSp(nPEs_ocn))
 
-  call open_file(unit,'input.nml',form=ASCII_FIE,acrion=READONLY_FILE)
+  call open_file(unit,'input.nml',form=ASCII_FILE,action=READONLY_FILE)
   read(unit,particle_driver_nml,iostat=io_status)
   call close_file(unit)
 
@@ -63,7 +81,6 @@ PROGRAM particle_driver
   call get_MOM_input(PF,dirs)
 
   if (file_exists(trim(dirs%restart_input_dir)//'ocean.res')) then
-    Mak
     call open_file(unit,trim(dirs%restart_input_dir)//'ocean.res',form=ASCII_FILE)
     read(unit,*) calendar_type; read(unit,*) date_init; read(unit,*) date
     call close_file(unit)
@@ -77,7 +94,7 @@ PROGRAM particle_driver
   call set_calendar_type(calendar_type)
 
   time_start = set_time(0,days=0)
-  if (sum_date_init)>0) time_start=set_date(date_init(1),date_init(2), date_init(3), &
+  if (sum(date_init)>0) time_start=set_date(date_init(1),date_init(2), date_init(3), &
        date_init(4), date_init(5), date_init(6))
 
   if (sum(date)>0) then
@@ -90,10 +107,15 @@ PROGRAM particle_driver
   call MOM_domains_init(Grid%domain,PF)
   call MOM_io_init(PF)
   call hor_index_init(Grid%Domain, HI, PF, &
-                      local_indexing=.true.)
-  call verticalGridInit( PF, CS%GV )
+       local_indexing=.true.)
+
+  do n=1,nPEs_ocn
+    CS=>CSp(n)
+    call verticalGridInit( PF, CS%GV )
+  enddo
+
   call create_dyn_horgrid(dG,HI)
-  call clone_MOM_domain(G%Domain, dG%Domain)
+  call clone_MOM_domain(Grid%Domain, dG%Domain)
 
   is   = dG%isc   ; ie   = dG%iec  ; js   = dG%jsc  ; je   = dG%jec ; nz = GV%ke
   isd  = dG%isd   ; ied  = dG%ied  ; jsd  = dG%jsd  ; jed  = dG%jed
@@ -101,44 +123,37 @@ PROGRAM particle_driver
 
   do n=1,nPEs_ocn
     CS=>CSp(n)
-  ! Allocate and initialize space for primary MOM variables.
-    ALLOC_(CS%u(IsdB:IedB,jsd:jed,nz))   ; CS%u(:,:,:) = 0.0
-    ALLOC_(CS%v(isd:ied,JsdB:JedB,nz))   ; CS%v(:,:,:) = 0.0
-    ALLOC_(CS%h(isd:ied,jsd:jed,nz))     ; CS%h(:,:,:) = GV%Angstrom
+    ! Allocate and initialize space for primary MOM variables.
+    allocate(CS%u(IsdB:IedB,jsd:jed,nz))   ; CS%u(:,:,:) = 0.0
+    allocate(CS%v(isd:ied,JsdB:JedB,nz))   ; CS%v(:,:,:) = 0.0
+    allocate(CS%h(isd:ied,jsd:jed,nz))     ; CS%h(:,:,:) = GV%Angstrom
   enddo
 
-  call MOM_initialize_fixed(G,CS%OBC,PF,.false.,dirs%output_directory)
-  call MOM_initialize_coord(GV, PF, .false., &
+  call MOM_initialize_fixed(dG,CS%OBC,PF,.false.,dirs%output_directory)
+  call MOM_initialize_coord(CS%GV, PF, .false., &
        dirs%output_directory, CS%tv, dG%max_depth)
 
   !   Shift from using the temporary dynamic grid type to using the final
   ! (potentially static) ocean-specific grid type.
   !   The next line would be needed if G%Domain had not already been init'd above:
   !     call clone_MOM_domain(dG%Domain, G%Domain)
-  call MOM_grid_init(G, param_file, HI, bathymetry_at_vel=bathy_at_vel)
-  call copy_dyngrid_to_MOM_grid(dG, G)
+  call MOM_grid_init(Grid, PF, HI)
+  call copy_dyngrid_to_MOM_grid(dG, Grid)
   call destroy_dyn_horgrid(dG)
 
 
-  call MOM_initialize_state(CS%u, CS%v, CS%h, CS%tv, Time, G, GV, PF, &
+  call MOM_initialize_state(CS%u, CS%v, CS%h, CS%tv, Time, Grid, CS%GV, PF, &
                             dirs, CS%restart_CSp, CS%ALE_CSp, CS%tracer_Reg, &
                             CS%sponge_CSp, CS%ALE_sponge_CSp, CS%OBC, Time_in)
 
-  call create_group_pass(CS%pass_uv_T_S_h, CS%u, CS%v, G%Domain)
+  call create_group_pass(CS%pass_uv_T_S_h, CS%u, CS%v, Grid%Domain)
 
-! Set up the parameters of the physical domain (i.e. the grid), G
-!  call set_grid_metrics(G, PF)
 
-! Set up the bottom depth, G%bathyT either analytically or from file
-! This also sets G%max_depth based on the input parameter MAXIMUM_DEPTH,
-! or, if absent, is diagnosed as G%max_depth = max( G%D(:,:) )
-!  call MOM_initialize_topography(G%bathyT, G%max_depth, G, PF)
-
-  node=>drifters
+!  node=>drifters
 
   !call mpp_domains_init
 
-  ! create particles framework
+!  create particles framework
 !  call particles_framework_ini(filename,topname,parts)
 !  grd=>parts%grd
 
